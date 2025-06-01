7080f2fc43f383f7ce165072afb9d9dc8e1bfe30
ra2dev <raramanouski@gmail.com>
2025-06-01 21:02:09 +0200

    Added protected-password-protection.

---
+++ examples/password-protect-nextjs-pages/middleware.ts
@@ -0,0 +1,33 @@
+import {NextRequest, NextResponse} from 'next/server'
+
+const BASIC_USER = process.env.BASIC_USER ?? ''
+const BASIC_PASS = process.env.BASIC_PASS ?? ''
+
+if(!BASIC_USER || !BASIC_PASS) {
+    throw new Error('Missing env.BASIC_USER or env.BASIC_PASS')
+}
+export const isAuthorized = (authHeader?: string): boolean => {
+    if (!authHeader || !authHeader?.startsWith('Basic ')) {
+        return false
+    }
+
+    const decoded = Buffer.from(authHeader.slice(6).trim(), 'base64').toString('utf8')
+    const [user, pass] = decoded.split(':')
+    return user === BASIC_USER && pass === BASIC_PASS
+}
+
+export function middleware(req: NextRequest) {
+    const authHeader = req.headers.get('authorization')
+    if (isAuthorized(authHeader)) {
+        return NextResponse.next()
+    }
+
+    return new Response('Auth Required', {
+        status: 401,
+        headers: {'WWW-Authenticate': 'Basic realm="Secure Area"'}
+    })
+}
+
+export const config = {
+    matcher: '/((?!_next).*)'
+}
