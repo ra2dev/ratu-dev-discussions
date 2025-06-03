29e21563a07f3ef495a62060dc8b55d36c61ba12
ra2dev <raramanouski@gmail.com>
2025-06-03 22:26:20 +0200

    Added links.

--- examples/password-protect-nextjs-pages/app/page.tsx
+++ examples/password-protect-nextjs-pages/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       Protected route
+       <h1>Example protected route</h1>
+       <a href="https://github.com/ra2dev/ratu-dev-discussions/tree/main/examples/password-protect-nextjs-pages">Code: https://github.com/ra2dev/ratu-dev-discussions/tree/main/examples/password-protect-nextjs-pages</a>
+       <a href="https://ratu.dev/snippets/password-protect-nextjs-pages">Blog: https://ratu.dev/snippets/password-protect-nextjs-pages</a>
       </main>
     </div>
   );
